58ad4982a194766541cba4b54a2ff2ecd45ddb34
Denys <pedan.denys@gmail.com>
2025-05-10 16:37:20 +1200

    Simple back camera stream

--- app/src/main/java/denys/diomaxius/camerax/ui/CameraPreview.kt
+++ app/src/main/java/denys/diomaxius/camerax/ui/CameraPreview.kt
@@ -1,8 +1,44 @@
 package denys.diomaxius.camerax.ui
 
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.LocalLifecycleOwner
 
 @Composable
-fun CameraPreview() {
+fun CameraPreview(
+    modifier: Modifier = Modifier
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
 
+    AndroidView(
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                scaleType = PreviewView.ScaleType.FIT_CENTER
+            }
+
+            val previewUseCase = Preview.Builder().build()
+            previewUseCase.setSurfaceProvider(previewView.surfaceProvider)
+
+            val listenableFuture = ProcessCameraProvider.getInstance(context)
+            listenableFuture.addListener({
+                val cameraProvider = listenableFuture.get()
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(lifecycleOwner, CameraSelector.DEFAULT_BACK_CAMERA, previewUseCase)
+            }, ContextCompat.getMainExecutor(context))
+
+            previewView
+        }
+    )
 }
